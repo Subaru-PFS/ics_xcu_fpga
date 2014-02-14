6e142e0e5164bbae3f27584a49140324efe9353e
Craig Loomis <cloomis@astro.princeton.edu>
2014-02-14 02:12:13 -0500

    add hint to run the xc3sprog program to load the xilinx output

--- src/build.sh
+++ src/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# This is the full build "stanza" from FPGA....cmd_log, for requesting a .bit file after doing a Clean Project
+# This is the full build "stanza" from FPGA....cmd_log, extracted after requesting a .bit file 
+# after doing a Clean Project.
 # The only changes were:
 #   - replaced "-intstyle ise" with "-inststyle xflow", which adjusts the tool outputs for "batch mode".
 #   - define and use $ROOT, $PROJ, $TARG
 #
-# I looked for Makefiles, but found either toys or thins so complex I could not trust them. But given
-# our specific needs we evidently could.
+# In other words, I have no idea what the zillians of options do.
+#
+# I webbed for Makefiles, but found either toys or things so complex I could not trust them. But given
+# our specific needs we almost certainly could.
 #
 
-ROOT="/home/pfs/ccdFPGA/src"
+ROOT="/home/pfs/ccdFPGA/src" # Just $PWD?
 PROJ="FPGA35S6045_TOP"
 TARG="xc6slx45t-fgg484-2"
 
@@ -19,3 +22,10 @@ map -filter "$ROOT/iseconfig/filter.filter" -intstyle xflow -p ${TARG} -w -logic
 par -filter "$ROOT/iseconfig/filter.filter" -w -intstyle xflow -ol high -mt 4 ${PROJ}_map.ncd ${PROJ}.ncd ${PROJ}.pcf 
 trce -filter $ROOT/iseconfig/filter.filter -intstyle xflow -v 3 -s 2 -n 3 -fastpaths -xml ${PROJ}.twx ${PROJ}.ncd -o ${PROJ}.twr ${PROJ}.pcf 
 bitgen -filter "iseconfig/filter.filter" -intstyle xflow -f ${PROJ}.ut ${PROJ}.ncd 
+
+echo
+echo "=========================================================================="
+echo "To load the FPGA, run something like 'xc3sprog -c xpc -p 0 -v ${PROJ}.bit'"
+echo 
+echo "   Note: 'xc3sprog -c' lists the bus types, and 'xc3sprog -c xpc' lists the Xilinx Platform Cable bus."
+echo "   Note: you might also need to 'setup xc3sprog' if the program is not found."
