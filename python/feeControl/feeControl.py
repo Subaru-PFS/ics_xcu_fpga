efab386c2b39b2d6c4534a9e6ba6ce68eebc64d5
Craig Loomis <cloomis@astro.princeton.edu>
2014-03-04 13:41:30 -0500

    Added a main() Pulled out the commands which require channel arguments into a subclass

--- python/feeControl/feeControl.py
+++ python/feeControl/feeControl.py
@@ -1,54 +1,81 @@
+#!/usr/bin/env python
+
 import argparse
 import logging
 import serial
+import sys
 
 class FeeSet(object):
-    def __init__(self, name, letter, subs=(), setCmd='s', getCmd='g'):
+    def __init__(self, name, letter, subs=(), setLetter='s', getLetter='g'):
         self.name = name
         self.letter = letter
         self.subs = subs
-        self.statusCache = {}
-        self.setCmd = setCmd
-        self.getCmd = getCmd
+        self.setLetter = setLetter
+        self.getLetter = getLetter
 
-    def setVal(self, subName, value=None):
-        if not self.setCmd:
+    def _genCmdString(self, cmdLetter, *parts):
+        allParts = [cmdLetter, self.letter].extend(parts)
+        return ','.join(allParts)
+
+    def setVal(self, subName, value):
+        """ Return the command string for a 'set' function. """
+
+        if not self.setLetter:
             raise RuntimeError("Cannot set %s(%s)!" % (self.name, subName))
         if subName not in self.subs:
             raise RuntimeError("Cannot set unknown %s (%s). Valid=%s" % (self.name, subName,
                                                                          self.subs))
         if value:
-            return "s%s,%s,%s" % (self.letter, subName, value)
+            return self._getCmdString(self.setLetter, subName, value)
         else:
-            return "s%s,%s" % (self.letter, subName)
+            return self._getCmdString(self.setLetter, subName)
 
-    def getVal(self, subName=None):
-        if not self.getCmd:
+    def getVal(self, subName):
+        """ Return the command string for a 'get' function. """
+
+        if not self.getLetter:
             raise RuntimeError("Cannot get %s(%s)!" % (self.name, subName))
 
         if subName:
             if subName not in self.subs:
                 raise RuntimeError("Cannot get unknown %s (%s). Valid=%s" % (self.name, subName,
                                                                              self.subs))
-            return "g%s,%s" % (self.letter, subName)
+            return self._getCmdString(self.getLetter, subName)
         else:
-            return "g%s" % (self.letter)
+            return self._getCmdString(self.getLetter)
 
-    def fetchStatus(self):
-        if not self.getCmd:
-            return dict()
-        for sub in subs:
+class FeeChannelSet(FeeSet):
+    def setVal(self, subName, channel, value):
+        """ Return the command string for a 'set' function. """
+
+        if not self.setLetter:
+            raise RuntimeError("Cannot set %s(%s)!" % (self.name, subName))
+        if subName not in self.subs:
+            raise RuntimeError("Cannot set unknown %s (%s). Valid=%s" % (self.name, subName,
+                                                                         self.subs))
+        if channel not in (0,1):
+            raise RuntimeError("Channel must be 0 or 1 %s(%s)!" % (self.name, subName))
             
-    def status(self, useCache=False):
-        if not useCache:
-            self.statusCache = {}
-        if not self.statusCache:
-            self.statusCache = self.fetchStatus()
+        return self._getCmdString(self.setLetter, subName, channel, value)
 
-        return self.statusCache
-        
+    def getVal(self, subName, channel):
+        """ Return the command string for a 'get' function. """
+
+        if not self.getLetter:
+            raise RuntimeError("Cannot get %s(%s)!" % (self.name, subName))
+
+        if channel not in (0,1):
+            raise RuntimeError("Channel must be 0 or 1 %s(%s)!" % (self.name, subName))
+            
+        if subName not in self.subs:
+            raise RuntimeError("Cannot get unknown %s (%s). Valid=%s" % (self.name, subName,
+                                                                         self.subs))
+
+        return self._getCmdString(self.getLetter, subName, channel)
+
+
 class FeeControl(object):
-    def __init__(self, ttyName='/dev/ttyS0', logLevel=logging.DEBUG):
+    def __init__(self, ttyName='/dev/ttyS0', logLevel=logging.WARNING):
         self.logger = logging.getLogger()
         self.logger.setLevel(logLevel)
         self.device = None
@@ -87,8 +114,8 @@ class FeeControl(object):
         self.commands = {}
 
         # Read from file....
-        self.commands['Revision'] = FeeSet('Revision', 'r', setCmd=None)
-        self.commands['PowerEnable'] = FeeSet('PowerEnable', 'e', ['all'])
+        self.commands['Revision'] = FeeSet('Revision', 'r', setLetter=None)
+        self.commands['Enable'] = FeeSet('Enable', 'e', ['all'])
 
         """
         //Read/calibrate supply voltages
@@ -108,9 +135,28 @@ class FeeControl(object):
         self.commands['Voltages'] = FeeSet('Voltages', 'v', 
                                            ['3V3M','3V3',
                                             '5VP','5VN','5VPpa', '5VNpa',
-                                            '12VP', '12VN', '24VN', '54VP'], 
-                                           setCmd='c')
+                                            '12VP', '12VN', '24VN', '54VP',
+                                            'all'], 
+                                           setLetter='c')
 
+        """
+        // Set/Get the CDS offset voltages 
+        #define setCDS_OS "so"
+        #define getCDS_OS "go"
+        # define co_0pos "0p" 
+        # define co_0neg "0n" 
+        # define co_1pos "1p" 
+        # define co_1neg "1n" 
+        # define co_2pos "2p" 
+        # define co_2neg "2n" 
+        # define co_3pos "3p" 
+        # define co_3neg "3n"
+        #  define co_0 "ch0" 
+        #  define co_1 "ch1"
+        """
+        self.commands['Offsets'] = FeeChannelSet('Offsets', 'o', 
+                                                 ['0p','1p','2p','3p',
+                                                  '0n','1n','2n','3n'])
         """
         //load/save bias presets
 
@@ -121,12 +167,10 @@ class FeeControl(object):
         #define pb_expose "expose"
         #define pb_wipe "wipe"
         #define po_offset "offset"
-
-
         """
         self.commands['Presets'] = FeeSet('Presets', 'p', 
                                           ["erase", "read", "expose", "wipe", "offset"],
-                                          getCmd='l')
+                                          getLetter='l')
 
     def sendSetCommand(self, setName, subName, value):
         cmdSet = self.commands[setName]
@@ -162,6 +206,9 @@ class FeeControl(object):
         except Exception as e:
             raise
     
+        if ret != fullCmd:
+            raise RuntimeError("command echo mismatch. sent :%r: rcvd :%r:" % (fullCmd, ret))
+ 
         return self.readResponse()
 
     def readResponse(self):
@@ -182,6 +229,37 @@ class FeeControl(object):
                 logging.debug("received :%s:" % (response))
                 return response
 
+    def setRaw(self, cmdStr):
+        """ Send a raw commmand string. Well we add the ~ and EOL. """
+        
+        print self.sendCommandStr(cmdStr)
+
+    def getRaw(self, cmdStr):
+        """ Send a raw commmand string. Well we add the ~ and EOL. """
+        
+        print self.sendCommandStr(cmdStr)
+
+    def getAll(self):
+        pass
+
+
+def main(argv):
+    if isinstance(argv, basestring):
+        argv = argv.split()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', type=bool)
+    parser.add_argument('-r', '--raw', type=str)
+
+    args = parser.parse_args(argv)
+
+    logLevel = logging.DEBUG if args.debug else logging.WARN
+
+    fee = FeeControl(logLevel=logLevel)
+    if args.raw:
+        print fee.getRaw(args.raw)
     
-def test1():
-    fee = FeeControl()
+
+if __name__ == "__main__":
+    main(sys.argv)
+
