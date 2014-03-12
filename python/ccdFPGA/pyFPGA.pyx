69331949c0bb18e93ad25f7266f2991663654484
Craig Loomis <cloomis@astro.princeton.edu>
2014-03-12 10:46:15 -0400

    Add FPGA-order to detector-order pixel remapping

--- python/ccdFPGA/pyFPGA.pyx
+++ python/ccdFPGA/pyFPGA.pyx
@@ -52,7 +52,7 @@ cdef class FPGA:
         return image
 
     cpdef readImage(self, int nrows=4240, int ncols=536, int namps=8, 
-                    doTest=False, debugLevel=1,
+                    doTest=False, debugLevel=1, doAmpMap=True,
                     rowFunc=None, rowFuncArgs=None):
         """ Configure and read out a detector. 
 
@@ -61,11 +61,13 @@ cdef class FPGA:
         nrows : int, optional
            The number of rows in the image. Default=4240
         ncols : int, optional
-           The number of columns in the image. Note that this per amp. Default=536
+           The number of columns in the image. Note that this is per amp. Default=536
         namps : int, optional
            The number of amps in the image. So the number of pixels is ncols * namps. Default=8
         doTest : bool, optional
-           If true, return a BEE-generated synthetic image.
+           If set True, return an FPGA-generated synthetic image.
+        doAmpMap : bool, optional
+           If set False, do not remap the pixels from FPGA readout order to detector order
         rowFuncArgs : dict, optional
            If set and rowFunc is to be called, these are added to the rowFunc keyword arguments
         rowFunc : callable, optional
@@ -92,6 +94,8 @@ cdef class FPGA:
         # Yes, magic -- look at the cython manual...
         cdef numpy.ndarray[numpy.uint16_t, ndim=2, mode="c"] image = numpy.zeros((nrows,ncols*namps), 
                                                                                  dtype='u2')
+        cdef numpy.ndarray[numpy.uint16_t, ndim=1, mode="c"] rowImage = numpy.zeros((ncols*namps), 
+                                                                                    dtype='u2')
         cdef uint32_t dataCrc, fpgaCrc
         cdef int row_i
 
@@ -102,7 +106,7 @@ cdef class FPGA:
 
         configureForReadout(doTest, nrows, ncols)
         for row_i in range(nrows):
-            ret = readLine(ncols*namps, &image[row_i,0], &dataCrc, &fpgaCrc)
+            ret = readLine(ncols*namps, &rowImage[0], &dataCrc, &fpgaCrc)
             if dataCrc != fpgaCrc:
                 errorMsg = ("CRC mismatch: FPGA: 0x%08x calculated: 0x%08x. FPGA CRC MUST start with 0xccc0000\n" %
                             (fpgaCrc, dataCrc))
@@ -111,6 +115,13 @@ cdef class FPGA:
             else:
                 errorMsg = "OK?"
 
+            
+            if doAmpMap:
+                cdef int c_i, a_i
+                for a_i in range(namps):
+                    for c_i in range(ncols):
+                        image[row_i,a_i*ncols + c_i] = rowImage[c_i*namps + a_i]
+                    
             if rowFunc:
                 rowFunc(row_i, image, error=errorMsg, 
                         fpgaCrc=fpgaCrc, dataCrc=dataCrc,
