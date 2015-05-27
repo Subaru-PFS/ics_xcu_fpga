a6c25bd2c9e11285d44e93414623e5652b32bffd
Craig Loomis <cloomis@astro.princeton.edu>
2015-05-27 07:12:58 -0400

    Add printPower()

--- python/power/powerUdp.py
+++ python/power/powerUdp.py
@@ -11,6 +11,20 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP_IPsock.bind((UDP_IP, UDP_PORT))
 sock.bind(('', 1025))
 
+def printPower(port):
+    if port != '4':
+        return
+
+    A = dataStore.get('I%s' % (port), None)
+    V = dataStore.get('V%s' % (port), None)
+
+    if A is None or V is None:
+        W = None
+    else:
+        W = float(A) * float(V)
+
+    print("Port %s: %s %s %s" % (port, V, A, W))
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     now = time.time()
@@ -23,9 +37,12 @@ while True:
         continue
 
     lastVal = dataStore.get(key, None)
-    if val != lastVal:
-        print("%0.2f %05s %s    %s" % (now, key, val, lastVal))
-
     dataStore[key] = val
+
+    if val != lastVal or lastVal is None:
+        if key[0] in {'V','I'}:
+            printPower(key[1:])
+#            print("%0.2f %05s %s    %s" % (now, key, val, lastVal))
+
 
 
