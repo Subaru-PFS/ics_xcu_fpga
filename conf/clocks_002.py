d17b56e38d6a84d47e55229fda43be69d3b44ec9
Craig Loomis <cloomis@astro.princeton.edu>
2015-11-05 07:14:47 -0500

    Added wipeClocks, readNoSerialClocks

--- conf/clocks_002.py
+++ conf/clocks_002.py
@@ -99,3 +99,118 @@ def readClocks(tickTime=40e-9):
 
     return pre, pix, post
 
+def readNoSerialClocks(tickTime=40e-9):
+    pre = Clocks(tickTime)
+    pre.changeFor(duration=120,
+                  turnOn= [P1,P3,CNV])
+    
+    pix = Clocks(tickTime, initFrom=pre)
+    pix.changeFor(duration=8,
+                  turnOn= [RG,DCR,IR,SCK])
+
+    pix.changeFor(duration=8,
+                  turnOn=[SW])
+
+    pix.changeFor(duration=8,
+                  turnOff=[SCK,RG])
+
+    pix.changeFor(duration=8,
+                  turnOff=[IR])
+
+    pix.changeFor(duration=6,
+                  turnOff=[DCR])
+
+    pix.changeFor(duration=2,
+                  turnOff=[CNV])
+
+    pix.changeFor(duration=120,
+                  turnOn= [I_M])
+
+    pix.changeFor(duration=2,
+                  turnOff=[I_M])
+
+    pix.changeFor(duration=6,
+                  turnOff=[SW])
+
+    pix.changeFor(duration=120,
+                  turnOn=[I_P])
+
+    pix.changeFor(duration=4,
+                  turnOff=[I_P])
+
+    pix.changeFor(duration=56,
+                  turnOn= [CNV])
+
+    post = Clocks(tickTime, initFrom=pix)
+    post.changeFor(duration=1000,
+                   turnOff=[P1],
+                   turnOn= [RG,IR,DCR])
+
+    post.changeFor(duration=1000,
+                   turnOn= [P2,TG,CRC])
+
+    post.changeFor(duration=1000,
+                   turnOff=[P3,CRC])
+
+    post.changeFor(duration=1000,
+                   turnOn=[P1])
+
+    post.changeFor(duration=1000,
+                   turnOff=[P2,TG])
+
+    post.changeFor(duration=1000,
+                   turnOn=[P3])
+
+    post.changeFor(duration=50,
+                   turnOff=[RG,IR])
+
+    post.changeFor(duration=2,
+                   turnOff= [DCR])
+
+    return pre, pix, post
+
+def wipeClocks(tickTime=40e-9):
+    pre = Clocks(tickTime)
+    pre.changeFor(duration=120,
+                  turnOn= [P1,P3,S1])
+
+    pix = Clocks(tickTime, initFrom=pre)
+    pix.changeFor(duration=16,
+                  turnOff=[S1],
+                  turnOn= [S2,SW,RG,DCR,IR])
+
+    pix.changeFor(duration=8,
+                  turnOff=[RG])
+
+    pix.changeFor(duration=24,
+                  turnOff=[S2,SW],
+                  turnOn= [S1])
+
+    post = Clocks(tickTime, initFrom=pix)
+    post.changeFor(duration=1000,
+                   turnOff=[P1],
+                   turnOn= [RG,IR,DCR])
+
+    post.changeFor(duration=1000,
+                   turnOn= [P2,TG])
+
+    post.changeFor(duration=1000,
+                   turnOff=[P3])
+
+    post.changeFor(duration=1000,
+                   turnOn=[P1])
+
+    post.changeFor(duration=1000,
+                   turnOff=[P2,TG])
+
+    post.changeFor(duration=1000,
+                   turnOn=[P3])
+
+    post.changeFor(duration=50,
+                   turnOff=[RG,IR])
+
+    post.changeFor(duration=2,
+                   turnOff= [DCR])
+
+    return pre, pix, post
+
