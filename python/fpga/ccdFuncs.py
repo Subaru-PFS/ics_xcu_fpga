fe9723c931de8bdeec0cde30e26f941a5f77fc57
Craig Loomis <cloomis@astro.princeton.edu>
2015-12-21 19:19:49 -0500

    Add routines to control Optics Lab hardware.

--- python/fpga/ccdFuncs.py
+++ python/fpga/ccdFuncs.py
@@ -8,7 +8,7 @@ import socket
 import sys
 import time
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 import fitsio
@@ -48,14 +48,65 @@ def getWipeClocks():
     
     return clocks.wipe.wipeClocks
 
-def pulseShutter(stime):
+def pulseShutter(stime, wavelength=None):
     host = '127.0.0.1'
     port = 10101
     
     print "pulsing shutter for %g seconds..." % (stime)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    s.send('shut %g\n' % (stime))
+    if wavelength is None:
+        s.send('shut %g\n' % (stime))
+    else:
+        s.send('shut %g %g\n' % (stime, wavelength))
+        
+    data = s.recv(1024)
+    if data[:2] != 'OK':
+        raise RuntimeError('something went wrong, got %s' % (data))
+    flux = float(data.split()[-1])
+    current = float(data.split()[-2])
+    
+    s.close()
+    
+    return stime, flux, current, flux*stime
+
+def monoSetWavelength(wave):
+    host = '127.0.0.1'
+    port = 50000
+    
+    print "setting wavelength to %g..." % (wave)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.send('wave %d\n' % (wave))
+    data = s.recv(1024)
+    s.close()
+
+    if data != 'moved to %4d' % (wave):
+        raise RuntimeError('failed to adjust monochrometer: %s' % (data))
+
+    return wave
+
+def monoSetSlitwidth(mm):
+    host = '127.0.0.1'
+    port = 50000
+    
+    print "setting slit width to %g mm..." % (mm)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.send('slit %g\n' % (mm))
+    data = s.recv(1024)
+    s.close()
+    
+    return data
+
+def monoSetLamp(lampID):
+    host = '127.0.0.1'
+    port = 50000
+    
+    print "setting lamp to %s..." % (lampID)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.send('lamp %d\n' % (lampID))
     data = s.recv(1024)
     s.close()
     
