deb772f43c0d8aea5f3039b8d4de80bfb3fd9a5c
Craig Loomis <cloomis@astro.princeton.edu>
2015-12-21 19:18:39 -0500

    Start geometry management.

--- python/fpga/geom.py
+++ python/fpga/geom.py
@@ -1,22 +1,79 @@
+import itertools
 import numpy as np
 
 import fitsio
 
 class Exposure(object):
-    def __init__(self, nccds=2):
+    def __init__(self, obj=None, nccds=2, copyExposure=False):
         self.nccds = nccds
         self._setDefaultGeometry()
+
+        if obj is None:
+            self.rawImage = None
+            self.image = None
+            self.header = dict()
+        elif isinstance(obj, Exposure):
+            self.rawImage = obj.rawImage
+            self.image = obj.image
+            self.header = obj.header
+            
+            if copyExposure:
+                self.image = self.image.copy()
+                self.header = self.header.copy()
+        elif isinstance(obj, basestring):
+            self.rawImage, self.header = fitsio.read(obj, header=True)
+            self.image = self.fixEdgeColsBug(self.rawImage)
+
+        elif isinstance(obj, np.ndarray):
+            self.rawImage = None
+            self.image = obj.copy() if copyExposure else obj
+            self.header = dict()
+        else:
+            raise RuntimeError("do not know how to construct from a %s" % (type(obj)))
+
+
+    def fixEdgeColsBug(self, image):
+
+        if ('CPL has not fixed the outer columns and rows yet'):
+            fixedImage1 = np.ndarray(shape=image.shape, dtype=image.dtype)
+            fixedImage1[:,:-1] = image[:,1:]
+            fixedImage1[:,-1] = image[:,0]
+
+            fixedImage = np.ndarray(shape=image.shape, dtype=image.dtype)
+            fixedImage[:-1,:] = fixedImage1[1:,:]
+            fixedImage[-1,:] = fixedImage1[0,:]
+
+            return fixedImage
+        else:
+            return image
         
     def _setDefaultGeometry(self):
         self.ampRows = 4224
         self.ampCols = 520
-        self.overRows = 100
+        self.overRows = 76
         self.overCols = 32
-        self.leadinCols = 10
-        self.leadinRows = 0
-        self.flipAmps = False
+        self.leadinCols = 8
+        self.leadinRows = 48
+        
+        self.flipAmps = True
         
         self.namps = 4 * self.nccds
+
+    @property
+    def expType(self):
+        return self.header.get('IMAGETYP', 'unknown').strip()
+    
+    @property
+    def expTime(self):
+        return self.header.get('EXPTIME', -1.0)
+
+    @property
+    def activeRows(self):
+        self.ampRows - self.leadinRows
+        
+    @property
+    def activeCols(self):
+        self.ampCols - self.leadinCols
         
     @property
     def nrows(self):
@@ -26,67 +83,176 @@ class Exposure(object):
     def ncols(self):
         return self.ampCols + self.overCols
 
-    def ampExtents(self, ampId):
-        x0 = ampId*self.ncols + self.leadinCols
-        x1 = (ampId+1)*self.ampCols
+    def ampExtents(self, ampId, leadingCols=False, leadingRows=False):
+        x0 = ampId*self.ncols + self.leadinCols*(not leadingCols)
+        x1 = ampId*self.ncols + self.ampCols
 
         if not self.flipAmps or ampId%2 == 0:
             xr = slice(x0, x1)
         else:
             xr = slice(x1-1, x0-1, -1)
 
-        yr = slice(self.leadinRows, self.ampRows)
+        yr = slice(self.leadinRows*(not leadingRows), self.ampRows)
 
         return yr, xr
 
-    def ampImage(self, im, ampId):
-        yr, xr = self.ampExtents(ampId)
-    
+    def ampImage(self, ampId, im=None, leadingCols=False, leadingRows=False):
+        if im is None:
+            im = self.image
+            
+        yr, xr = self.ampExtents(ampId, leadingRows=leadingRows, leadingCols=leadingCols)
+
         return im[yr, xr]
 
-    def overscanExtents(self, ampId):
+    def overscanCols(self, ampId, leadingRows=False, overscanRows=False):
         x0 = ampId*self.ncols + self.ampCols
-        x1 = (ampId+1)*self.ncols
+        x1 = x0 + self.overCols
 
         xr = slice(x0, x1)
-        yr = slice(self.leadinRows, self.ampRows)
+        yr = slice(self.leadinRows*(not leadingRows),
+                   self.ampRows + self.overRows*(overscanRows))
 
         return yr, xr
 
-    def overscanImage(self, im, ampId):
-        yr, xr = self.overscanExtents(ampId)
+    def overscanRows(self, ampId, leadingCols=False, overscanCols=False):
+        x0 = ampId*self.ncols + self.leadinCols*(not leadingCols)
+        x1 = ampId*self.ncols + self.ampCols + self.overCols*(overscanCols)
+
+        xr = slice(x0, x1)
+        yr = slice(self.ampRows, self.ampRows + self.overRows)
+
+        return yr, xr
+
+    def overscanRowImage(self, ampId, im=None, leadingCols=False, overscanCols=False):
+        if im is None:
+            im = self.image
+        yr, xr = self.overscanRows(ampId, leadingCols=leadingCols, overscanCols=overscanCols)
     
         return im[yr, xr]
 
-    def coreAmpImage(self, im, ampId):
-        ampImg = self.ampImage(im, ampId)
+    def overscanColImage(self, ampId, im=None, leadingRows=False, overscanRows=False):
+        if im is None:
+            im = self.image
+        yr, xr = self.overscanCols(ampId, leadingRows=leadingRows, overscanRows=overscanRows)
+    
+        return im[yr, xr]
+
+    def coreAmpImage(self, ampId, im=None, offset=(0,0)):
+        if im is None:
+            im = self.image
+        ampImg = self.ampImage(ampId, im=im)
+
+        amph, ampw = ampImg.shape
+        hslice = slice(int(amph/2.0 - 50 + offset[0]),
+                       int(amph/2.0 + 50 + offset[0]))
+        wslice = slice(int(ampw/2.0 - 50 + offset[1]),
+                       int(ampw/2.0 + 50 + offset[1]))
+    
+        return ampImg[hslice, wslice]
+
+    def coreOverscanColImage(self, ampId, im=None):
+        if im is None:
+            im = self.image
+        ampImg = self.overscanColImage(ampId, im=im)
 
         amph, ampw = ampImg.shape
         hslice = slice(int(amph/2.0 - 50), int(amph/2.0 + 50))
+        wslice = slice(5, -1)
+    
+        return ampImg[hslice, wslice]
+
+    def coreOverscanRowImage(self, ampId, im=None):
+        if im is None:
+            im = self.image
+        ampImg = self.overscanRowImage(ampId, im=im)
+
+        amph, ampw = ampImg.shape
+        hslice = slice(5, -1)
         wslice = slice(int(ampw/2.0 - 50), int(ampw/2.0 + 50))
     
         return ampImg[hslice, wslice]
 
-    def coreOverscanImage(self, im, ampId):
-        ampImg = self.overscanImage(im, ampId)
-
-        amph, ampw = ampImg.shape
-        hslice = slice(int(amph/2.0 - 50), int(amph/2.0 + 50))
-        wslice = slice(5, None)
-    
-        return ampImg[hslice, wslice]
-
-    def biasSubtract(self, im, byRow=False):
-        amps = []
+    def allAmpsImages(self, leadingCols=False, leadingRows=False):
+        ampIms = []
         
+        for i in range(self.namps):
+            ampIms.append(self.ampImage(i, leadingCols=leadingCols,
+                                        leadingRows=leadingRows))
+
+        return ampIms
+    
+    def allOverscanColImages(self, leadingRows=False, overscanRows=False):
+        osIms = []
+        
+        for i in range(self.namps):
+            osIms.append(self.overscanColImage(i, leadingRows=leadingRows,
+                                               overscanRows=overscanRows))
+
+        return osIms
+    
+    def allOverscanRowImages(self, leadingCols=False, overscanCols=False):
+        osIms = []
+        
+        for i in range(self.namps):
+            osIms.append(self.overscanRowImage(i, leadingCols=leadingCols,
+                                               overscanCols=overscanCols))
+
+        return osIms
+
+    def splitImage(self, doTrim=False, doFull=False):
+
+        if doTrim and doFull:
+            raise RuntimeError("cannot accept both doFull and doTrim")
+        
+        ampIms = []
+        osColIms = []
+        osRowIms = []
+
+        for a_i in range(self.namps):
+            if doTrim:
+                ampIm = self.coreAmpImage(a_i)
+                osColIm = self.coreOverscanColImage(a_i)
+                osRowIm = self.coreOverscanRowImage(a_i)
+            elif doFull:
+                ampIm = self.ampImage(a_i, leadingCols=True, leadingRows=True)
+                osColIm = self.overscanColImage(a_i, leadingRows=True)
+                osRowIm = self.overscanRowImage(a_i, leadingCols=True, overscanCols=True)
+            else:
+                ampIm = self.ampImage(a_i)
+                osColIm = self.overscanColImage(a_i)
+                osRowIm = self.overscanRowImage(a_i)
+
+            ampIms.append(ampIm)
+            osColIms.append(osColIm)
+            osRowIms.append(osRowIm)
+
+        return ampIms, osColIms, osRowIms
+
+    def biasSubtract(self, bias, im=None, corretLevels=True):
+        bias = Exposure(bias)
+        
+        if im is None:
+            im = self.image
+        im = im.astype('i4') - bias.image
+        
+        return Exposure(im).finalImage()
+        
+    def biasSubtractOne(self, im=None, byRow=False):
+        if im is None:
+            im = self.image
+        amps = []
+
         for a_i in range(8):
-            amps.append(self.biasSubtractAmp(im, a_i, byRow=byRow))
+            amps.append(self.biasSubtractAmp(a_i, im=im, byRow=byRow))
         
         return np.hstack(amps)
-        
-    def biasSubtractAmp(self, im, ampId, byRow=False):
 
-        ampIm = self.ampImage(im, ampId)
+    
+    def biasSubtractAmp(self, ampId, im=None, byRow=False):
+        if im is None:
+            im = self.image
+
+        ampIm = self.ampImage(ampId, im=im)
         osYr, osXr = self.overscanExtents(ampId)
 
         if byRow:
@@ -103,13 +269,10 @@ class Exposure(object):
 
         return ampIm.astype('i4') - imMed
     
-def stackIms(ims):
-    stack = np.dstack(ims)
-    return np.median(stack, axis=2)
-
-def readIms(filenames):
-    return [fitsio.read(f) for f in filenames]
-
+    def finalImage(self):
+        ampImages = self.allAmpsImages()
+        return np.hstack(ampImages)
+    
 def clippedStats(a, nsig=3.0, niter=5):
     a = a.reshape(-1)
     keep = np.ones(a.size, dtype=np.bool)
@@ -128,218 +291,57 @@ def clippedStats(a, nsig=3.0, niter=5):
                                                                                nkeep0, nkeep1)
     return a[keep].mean(), a[keep].std(), float(nkeep1)/a.size
 
+def clippedStack(flist, dtype='i4'):
+    """ Return the median of a stack of images.
 
-def splitImage(im, doTrim=False):
-    exp = Exposure()
+    Notes:
     
-    ampIms = []
-    osIms = []
+    The individual images are median-subtracted before medianed together, and 
+    the median offset is added back to the result.
 
-    for a_i in range(exp.namps):
-        if doTrim:
-            ampIm = exp.coreAmpImage(im, a_i)
-            osIm = exp.coreOverscanImage(im, a_i)
-        else:
-            ampIm = exp.ampImage(im, a_i)
-            osIm = exp.overscanImage(im, a_i)
-
-        ampIms.append(ampIm)
-        osIms.append(osIm)
-
-    return ampIms, osIms
-
-def imStats(im):
-    if isinstance(im, basestring):
-        im = fitsio.read(im)
-
-    exp = Exposure()
+    All images must match the images type and exposure time of the first image.
     
-    ampIms, osIms = splitImage(im, doTrim=True)
+    """
 
-    stats = np.zeros(shape=exp.namps,
-                     dtype=([('signal','f4'),
-                             ('sqrtSig', 'f4'),
-                             ('bias', 'f4'),
-                             ('readnoise', 'f4'),
-                             ('readnoiseM', 'f4'),
-                             ('shotnoise', 'f4'),
-                             ('shotnoiseM', 'f4'),
-                             ('gain', 'f4'),
-                             ('gainM', 'f4'),
-                             ('noise', 'f4')]))
+    exp = Exposure(flist[0])
+    imshape = exp.image.shape
+    imtype = exp.header['IMAGETYP']
+    exptime = exp.header['EXPTIME']
 
-    for a_i in range(8):
-        ampIm = ampIms[a_i]
-        osIm = osIms[a_i]
-        ampSig = np.median(ampIm)
-        osSig = np.median(osIm)
-        stats[a_i]['signal'] = signal = ampSig - osSig
-        stats[a_i]['sqrtSig'] = np.sqrt(signal)
-        stats[a_i]['bias'] = osSig
+    stack = np.zeros((len(flist), imshape[0], imshape[1]), dtype=dtype)
+    meds = np.zeros(len(flist))
 
-        sig1 = (0.741/np.sqrt(2)) * np.subtract.reduce(np.percentile(ampIm, [75,25]))
-        sig2 = (0.741/np.sqrt(2)) * np.subtract.reduce(np.percentile(osIm, [75,25]))
-        _, trusig1, _ = clippedStats(ampIm) / np.sqrt(2)
-        _, trusig2, _ = clippedStats(osIm) / np.sqrt(2)
-        stats[a_i]['readnoise'] = sig2
-        stats[a_i]['readnoiseM'] = trusig2
+    for i, f in enumerate(flist):
+        exp = Exposure(f)
+        if exp.header['IMAGETYP'] != imtype or exp.header['EXPTIME'] != exptime:
+            print("%s: unexpected imagetyp: %s(%0.2f) vs %s(%0.2f)"
+                  % (f, 
+                     exp.header['IMAGETYP'], exp.header['EXPTIME'],
+                     imtype, exptime))
+            
+        meds[i] = np.median(exp.image)
+        stack[i] = exp.image - meds[i]
 
-        stats[a_i]['shotnoise'] = sig = np.sqrt(np.abs(sig1**2 - sig2**2))
-        stats[a_i]['shotnoiseM'] = trusig = np.sqrt(np.abs(trusig1**2 - trusig2**2))
-
-        stats[a_i]['gain'] = gain = signal/sig**2
-        stats[a_i]['gainM'] = signal/trusig**2
-        stats[a_i]['noise'] = sig2*gain
-        
-    return ampIms, osIms, stats
-
-def flatStats(f1, f2):
-    if isinstance(f1, basestring):
-        f1 = fitsio.read(f1)
-    if isinstance(f2, basestring):
-        f2 = fitsio.read(f2)
-
-    exp = Exposure()
+    medimg = np.median(stack, axis=0)
+    return medimg + np.median(meds), meds
+                                                                                                            
+def finalImage(exp, bias=None, dark=None, flat=None):
+    exp = Exposure(exp)
+    return exp.biasSubtract(bias)
     
-    f1AmpIms, f1OsIms = splitImage(f1, doTrim=True)
-    f2AmpIms, f2OsIms = splitImage(f2, doTrim=True)
-    diffAmpIms, diffOsIms = splitImage(f2-f1.astype('f4'), doTrim=True)
+def constructImage(ampIms, osColIms=None, osRowIms=None):
+    orderedIms = []
 
-    stats = np.zeros(shape=exp.namps,
-                     dtype=([('signal','f4'),
-                             ('sqrtSig', 'f4'),
-                             ('bias', 'f4'),
-                             ('readnoise', 'f4'),
-                             ('readnoiseM', 'f4'),
-                             ('shotnoise', 'f4'),
-                             ('shotnoiseM', 'f4'),
-                             ('gain', 'f4'),
-                             ('gainM', 'f4'),
-                             ('noise', 'f4')]))
+    if osColIms is not None:
+        for i in range(len(ampIms)):
+            orderedIms.append(np.concatenate((ampIms[i], osColIms[i]), axis=1))
+    else:
+        orderedIms = ampIms[:]
 
-    for a_i in range(8):
-        _s1 = np.median(f1AmpIms[a_i]) - np.median(f1OsIms[a_i])
-        _s2 = np.median(f2AmpIms[a_i]) - np.median(f2OsIms[a_i])
-        stats[a_i]['signal'] = signal = (_s1+_s2)/2
-        stats[a_i]['sqrtSig'] = np.sqrt(signal)
-        stats[a_i]['bias'] = (np.median(f1OsIms[a_i]) + np.median(f2OsIms[a_i]))/2
+    if osRowIms is not None:
+        for i in range(len(orderedIms)):
+            orderedIms[i] = np.concatenate((orderedIms[i], osRowIms[i]), axis=0)
 
-        ampIm = diffAmpIms[a_i]
-        osIm = diffOsIms[a_i]
-        
-        sig1 = (0.741/np.sqrt(2)) * np.subtract.reduce(np.percentile(ampIm, [75,25]))
-        sig2 = (0.741/np.sqrt(2)) * np.subtract.reduce(np.percentile(osIm, [75,25]))
-        _, trusig1, _ = clippedStats(ampIm) / np.sqrt(2)
-        _, trusig2, _ = clippedStats(osIm) / np.sqrt(2)
-        stats[a_i]['readnoise'] = sig2
-        stats[a_i]['readnoiseM'] = trusig2
+    ret = np.concatenate(orderedIms, axis=1)
+    return ret
 
-        stats[a_i]['shotnoise'] = sig = np.sqrt(np.abs(sig1**2 - sig2**2))
-        stats[a_i]['shotnoiseM'] = trusig = np.sqrt(np.abs(trusig1**2 - trusig2**2))
-
-        stats[a_i]['gain'] = gain = signal/sig**2
-        stats[a_i]['gainM'] = signal/trusig**2
-        stats[a_i]['noise'] = sig2*gain
-        
-    return diffAmpIms, diffOsIms, stats
-
-def printStats(stats):
-
-    po = np.get_printoptions()
-    np.set_printoptions(formatter=dict(float=lambda f: "%0.2f" % (f)))
-    
-    print("amp readnoise readnoiseM  gain  gainM    signal    bias sig^0.5 shotnoise shotnoiseM noise\n")
-
-    for i in range(8):
-        print(str(i) + "   %(readnoise)9.2f %(readnoiseM)9.2f %(gain)6.2f %(gainM)6.2f %(signal)9.2f %(bias)7.2f %(sqrtSig)7.2f %(shotnoise)9.2f %(shotnoiseM)9.2f %(noise)6.2f" % stats[i])
-
-    np.set_printoptions(**po)
-        
-def dispStats(f1,f2, d, frames=(2,3,4,5)):
-    ampIms, osIms, stats = flatStats(f1,f2)
-
-
-    d.set('frame hide all')
-    for _f in frames:
-        d.set('frame show %d' % (_f))
-    d.set('tile row')
-    
-    d.set('frame %d' % frames[0])
-    d.set('scale xscale')
-    d.set('zoom to fit')
-    
-    ampIm = np.hstack(ampIms)
-    d.set_np2arr(ampIm)
-
-    d.set('frame %d' % frames[1])
-    d.set('scale xscale')
-    d.set('zoom to fit')
-    
-    osIm = np.hstack(osIms)
-    d.set_np2arr(osIm)
-
-    d.set('frame %d' % frames[2])
-    d.set('scale xscale')
-    d.set('zoom to fit')
-    
-    sigIm = np.hstack([ampIms[i] - np.median(osIms[i]) for i in range(8)])
-    d.set_np2arr(sigIm)
-
-    d.set('frame %d' % frames[3])
-    d.set('scale xscale')
-    d.set('zoom to fit')
-    
-    osIm = np.hstack([osIms[i] - np.median(osIms[i]) for i in range(8)])
-    d.set_np2arr(osIm)
-
-    return ampIms, osIms, stats
-    
-"""
-To calculate the gain and noise:
-
-Take two identical flats. Do
-stats in a standard central region, say 100x100.
-Find the median signal level in this region and
-subtract the median signal in the overscan. The
-average of these two is the SIGNAL, S
-
-Subtract them
-
-Find the quartiles in this same region. For
-gaussian noise, the sigma is .741*(3rdqt - 1stqt),
-but we have taken a difference, so
-Sig1 = (0.741/sqrt(2))*(3rdqt - 1stqt)
-
-is the `sigma' in the illuminated part. Also calculate
-the 3-sigma clipped standard deviation and divide it
-by sqrt(2). This is Trusig1.
-
-In the
-overscan region, do the same
-
-Sig2 = (0.741/sqrt(2))*(3rdqt - 1stqt),
-
-This is the read noise in ADU.
-
-calculate Trusig2 likewise.
-
-Then
-
-Sig = sqrt (Sig1^2 - Sig2^2)
-
-is the shot noise in the signal, and
-
-Trusig = sqrt(Trusig1^2 - Trusig2^2)
-
-is the `real' sigma. Compare.
-
-
-Then the inverse gain G in e-/ADU is
-
-G = S/(Sig^2). Calculate for both the
-sigma based on quartiles and the one
-based on moments.
-
-The noise in electrons is Sig2*G.
-
-"""
