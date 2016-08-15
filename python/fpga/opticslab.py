cffb349fcb913b46f10e46fee62f1880ac9f23a4
Craig Loomis <cloomis@astro.princeton.edu>
2016-08-15 11:33:39 -0400

    Adapt to new PC server.

--- python/fpga/opticslab.py
+++ python/fpga/opticslab.py
@@ -1,72 +1,106 @@
 import socket
 import time
 
-def pulseShutter(stime, wavelength=None):
+def opticsLabCommand(cmdStr):
     host = '127.0.0.1'
-    port = 10101
+    port = 50000
 
-    t0 = time.time()
-    print "pulsing shutter for %g seconds..." % (stime)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    if wavelength is None:
-        s.send('shut %g\n' % (stime))
-    else:
-        s.send('shut %g %g\n' % (stime, wavelength))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send('%s\n' % (cmdStr))
+        data = s.recv(1024)
+        data = data.strip()
+    finally:
+        s.close()
         
-    data = s.recv(1024)
-    if data[:2] != 'OK':
-        raise RuntimeError('something went wrong, got %s' % (data))
-    flux = float(data.split()[-1])
-    current = float(data.split()[-2])
+    return data
+
+def pulseShutter(stime):
+    """ Move the cold shutter in or out.
+
+    Args:
+       pos : 'in' or 'out'
+    """
+    t0 = time.time()
+    ret = opticsLabCommand('pulse %g' % (stime))
+    parts = ret.split()
+    if parts[0] != 'OK' or parts[1] != 'pulse':
+        raise RuntimeError('something went wrong, got %r' % (ret))
+
+    flux = float(parts[3])
+    current = float(parts[4])
+    wavelength = float(parts[5])
+    slitwidth = float(parts[6])
     t1 = time.time()
 
     if t1-t0 < stime:
         raise RuntimeError('remote pulseShutter took less than the requested time!')
     
-    s.close()
-    
-    return stime, flux, current, flux*stime
+    return stime, flux, current, wavelength, slitwidth
 
 def monoSetWavelength(wave):
-    host = '127.0.0.1'
-    port = 50000
-    
-    print "setting wavelength to %g..." % (wave)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.send('wave %d\n' % (wave))
-    data = s.recv(1024)
-    s.close()
-
-    if data != 'moved to %4d' % (wave):
-        raise RuntimeError('failed to adjust monochrometer: %s' % (data))
+    ret = opticsLabCommand('wave %d' % (wave))
+    if ret != 'moved to %4.0f' % (wave):
+        raise RuntimeError('failed to adjust monochrometer: %r' % (ret))
 
     return wave
 
 def monoSetSlitwidth(mm):
-    host = '127.0.0.1'
-    port = 50000
-    
-    print "setting slit width to %g mm..." % (mm)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.send('slit %g\n' % (mm))
-    data = s.recv(1024)
-    s.close()
-    
-    return data
+    """ Set the monochrometer slit width
 
-def monoSetLamp(lampID):
-    host = '127.0.0.1'
-    port = 50000
+    Args:
+       mm : slit width.
+    """
+    ret = opticsLabCommand('slit %g' % (mm))
+    if ret != 'slit width %1.2f' % (mm):
+        raise RuntimeError('failed to adjust slit width: %r' % (ret))
+        
+    return mm
+
+def monoSetLamp(lamp):
+    """ Choose the monochrometer lamp.
+
+    Args:
+       lamp : 'arc' or 'qth'
+    """
+    knownLamps = {'qth', 'arc'}
+    if lamp not in knownLamps:
+        raise ValueError('%s is not one of: %s' % (lamp, knownLamps))
+    ret = opticsLabCommand('lamp %s' % (lamp))
+    if (ret != 'lamp %s' % (lamp)) and (ret != 'lamp already is %s' % (lamp)):
+        raise RuntimeError('failed to set lamp: %r' % (ret))
+        
+    return lamp
+
+def fe55(pos):
+    """ Move the Fe55 source in or out.
+
+    Args:
+       pos : 'in' or 'out'
+    """
+    knownPositions = {'in', 'out'}
+    if pos not in knownPositions:
+        raise ValueError('%s is not one of' % (pos, knownPositions))
+    ret = opticsLabCommand('iron' % (pos))
+    if (ret != 'iron %s' % (pos)):
+        raise RuntimeError('failed to move Fe55 source: %r' % (ret))
+        
+    return pos
+
+def coldShutter(pos):
+    """ Move the cold shutter in or out.
+
+    Args:
+       pos : 'in' or 'out'
+    """
     
-    print "setting lamp to %s..." % (lampID)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.send('lamp %d\n' % (lampID))
-    data = s.recv(1024)
-    s.close()
-    
-    return data
+    knownPositions = {'in', 'out'}
+    if pos not in knownPositions:
+        raise ValueError('%s is not one of' % (pos, knownPositions))
+    ret = opticsLabCommand('cold' % (pos))
+    if (ret != 'cold %s' % (pos)):
+        raise RuntimeError('failed to move cold shutter: %r' % (ret))
+        
+    return pos
 
