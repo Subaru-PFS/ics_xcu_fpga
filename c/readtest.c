2d695a0cb9e82f7fee1856fed3096609c14ee325
Craig Loomis <cloomis@astro.princeton.edu>
2017-03-30 10:47:11 -0400

    Use library device initialization.

--- c/readtest.c
+++ c/readtest.c
@@ -6,7 +6,7 @@
 #include<fcntl.h>
 #include<assert.h>
 
-#define FPGABASE 0xfe9ff000 // XXX change as needed
+#include "fpga.h"
 
 volatile unsigned int *fpga;
 volatile unsigned int dummy;
@@ -15,17 +15,10 @@ int main(void) {
 	int i, fd;
 
 	assert(4 == sizeof(unsigned int));
-	fd = open("/dev/mem", O_RDWR|O_SYNC);
-	if (fd == -1) {
-		perror("open(/dev/mem):");
-		return 0;
-	}
-	fpga = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, FPGABASE);
-	if (fpga == MAP_FAILED) {
-		perror("mmap:");
-		return 0;
-	}
 
+        configureFpga(0);
+        fpga = fpgaAddr();
+        
 	for (i=0; i<8000000; i++) {
 		dummy = *fpga;
 	}
