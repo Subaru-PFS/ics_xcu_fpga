def1bb8d01a2722459246cda8d861ff9c0df77a5
Craig Loomis <cloomis@astro.princeton.edu>
2017-05-30 12:16:16 -0400

    Add a routine to insert N non-reading clocks

--- python/clocks/read.py
+++ python/clocks/read.py
@@ -1,13 +1,48 @@
 from clocks import Clocks
 
-from clockIDs import *
+def insertIdlePixels(clks, cnt):
+    """ Insert a number of complete pixel clockings, without shift or conversion. 
+
+    Args:
+    -----
+    clks : a Clocks instance
+       The clocking which we insert into.
+    cnt : int
+       The number of pixels to insert..
+
+    We want to run the serial clocks during parallel clocking, so that
+    charge does not accumulate. Of course we don't want to actually _read_
+    the pixels.
+    """
+
+    for i in range(cnt):
+        clks.changeFor(duration=8,
+                       turnOn= [DCR,IR])
+
+        clks.changeFor(duration=8+8,
+                       turnOn=[SW])
+
+        clks.changeFor(duration=8,
+                       turnOff=[IR])
+
+        clks.changeFor(duration=6+2+108,
+                       turnOff=[DCR])
+
+        clks.changeFor(duration=20+108+16+32,
+                       turnOff=[SW])
+
+        clks.changeFor(duration=12,
+                       turnOn= [RG])
+
+        clks.changeFor(duration=12,
+                       turnOff= [RG])
 
 def readClocks():
     pre = Clocks()
     pre.changeFor(duration=120,
                   turnOn= [P1,P3,S1,CNV])
     
-    pix = Clocks(initFrom=pre)
+    pix = Clocks(initFrom=pre, logLevel=20)
     pix.changeFor(duration=8,
                   turnOff=[S1],
                   turnOn= [S2,DCR,IR,SCK])
@@ -49,31 +84,38 @@ def readClocks():
     pix.changeFor(duration=12,
                   turnOff= [RG])
 
-    par = Clocks(initFrom=pix)
-    par.changeFor(duration=1000,
-                  turnOff=[P1],
-                  turnOn= [RG,IR,DCR])
+    # We want each phase of the parallel clocking to take ~40us.
+    # We also want each phase to consist of an integral number of
+    # complete pixels clockings. With the current pixel time
+    # of 13.92 us, we make it 3.
+    #
+    pixTicks = pix.ticks[-1]
+    parPhasePixCnt = 3 # np.int(np.ceil(40000 / (pixTicks * 40)))
+    parPhaseTicks = pixTicks * parPhasePixCnt
 
-    par.changeFor(duration=1000,
-                  turnOn= [P2,TG,CRC])
+    par = Clocks(initFrom=pix, logLevel=20)
+    par.changeAt(at=0,
+                 turnOff=[P1])
+    insertIdlePixels(par, parPhasePixCnt)
+    
+    par.changeAt(at=1*parPhaseTicks,
+                 turnOn= [P2,TG,CRC])
+    insertIdlePixels(par, parPhasePixCnt)
 
-    par.changeFor(duration=1000,
-                  turnOff=[P3,CRC])
+    par.changeAt(at=2*parPhaseTicks,
+                 turnOff=[P3,CRC])
+    insertIdlePixels(par, parPhasePixCnt)
 
-    par.changeFor(duration=1000,
-                  turnOn=[P1])
+    par.changeAt(at=3*parPhaseTicks,
+                 turnOn=[P1])
+    insertIdlePixels(par, parPhasePixCnt)
 
-    par.changeFor(duration=1000,
-                  turnOff=[P2,TG])
+    par.changeAt(at=4*parPhaseTicks,
+                 turnOff=[P2,TG])
+    insertIdlePixels(par, parPhasePixCnt)
 
-    par.changeFor(duration=1000,
-                  turnOn=[P3])
-
-    par.changeFor(duration=50,
-                  turnOff=[RG,IR])
-
-    par.changeFor(duration=2,
-                  turnOff= [DCR])
+    par.changeAt(at=5*parPhaseTicks,
+                 turnOn=[P3])
 
     return pre, pix, par
 
