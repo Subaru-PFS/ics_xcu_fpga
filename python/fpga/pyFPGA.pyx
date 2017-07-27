6f3c678f4d62955707b5cee76a7842d17ed054fb
Craig Loomis <cloomis@astro.princeton.edu>
2017-07-27 18:59:42 -0400

    Allow setting the site.

--- python/fpga/pyFPGA.pyx
+++ python/fpga/pyFPGA.pyx
@@ -58,7 +58,8 @@ def printProgress(row_i, image, errorMsg="OK", everyNRows=100,
         logger.info("line %05d %s", row_i, errorMsg)
     
 cdef class FPGA:
-    def __cinit__(self, spectroId, dewarId, splitDetectors=False, adc18bit=1):
+    def __cinit__(self, spectroId, dewarId, splitDetectors=False,
+                  site=None, adc18bit=1):
         configureFpga(<const char *>0)
         self.adc18bit = adc18bit
 
